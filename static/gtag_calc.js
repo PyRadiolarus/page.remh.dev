9359f778fe9f15e196910e1e5401d35d9d659b37
PyRadiolarus <60543948+PyRadiolarus@users.noreply.github.com>
2022-03-05 08:21:37 +0900

    add js

---
+++ static/gtag_calc.js
@@ -0,0 +1,9 @@
+// Global site tag (gtag.js) - Google Analytics
+    document.write('<script async src="https://www.googletagmanager.com/gtag/js?id=G-SLLVKQBE42"></script>');    
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'G-SLLVKQBE42');
