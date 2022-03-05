742646a6fc8b405d74157f900dd954e1b3c28d3a
PyRadiolarus <60543948+PyRadiolarus@users.noreply.github.com>
2022-03-05 20:54:36 +0900

    change gtag

--- docs/js/gtag_calc.js
+++ docs/js/gtag_calc.js
@@ -1,9 +1,9 @@
 // Global site tag (gtag.js) - Google Analytics
-    document.write('<script async src="https://page.remh.dev/js/gtag_G-SLLVKQBE42.js"></script>');    
+    document.write('<script async src="https://www.googletagmanager.com/gtag/js?id=G-LJYP9H8NBZ"></script>');    
     window.dataLayer = window.dataLayer || [];
     function gtag(){
         dataLayer.push(arguments);
     }
     gtag('js', new Date());
 
-    gtag('config', 'G-SLLVKQBE42');
+    gtag('config', 'G-LJYP9H8NBZ');
